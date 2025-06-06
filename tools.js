6dda6ae32a730ecea96a2ac6f625d24794224818
Fatim-h <167449143+Fatim-h@users.noreply.github.com>
2025-06-06 17:34:20 +0500

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,7 +1,12 @@
-import { currentRows, currentCols } from './file.js';
-
-const { useState } = React;
-
 const root2 = ReactDOM.createRoot(document.getElementById("current_color"));
-const currentColor = "#cfe8fc";
-root2.render(<span>Color Hex: {currentColor}</span>);
+
+const input = document.getElementById("favcolor");
+const color_show = document.getElementById("color_show");
+
+root2.render(<span>Color Hex: {input.value}</span>);
+
+input.addEventListener("change", (e) => {
+  const currentColor = input.value;
+  color_show.style.backgroundColor = input.value;
+  root2.render(<span>Color Hex: {currentColor}</span>); 
+});

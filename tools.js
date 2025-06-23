badcdfeef6b8ca52c14d23e14bb8d4c8d0abcf7d
Fatim-h <167449143+Fatim-h@users.noreply.github.com>
2025-06-23 14:46:59 +0500

    Update tools.js for handleUndo() and handleRedo()

--- tools.js
+++ tools.js
@@ -10,3 +10,29 @@ input.addEventListener("change", (e) => {
   color_show.style.backgroundColor = input.value;
   root2.render(<span>Color Hex: {currentColor}</span>); 
 });
+
+//set global stacks
+window.undo_stack = [];
+window.redo_stack = [];
+
+function handleUndo() {
+  if (window.undo_stack.length > 0 && window.setCellColors) {
+    const currentState = window.getCellColors();
+    const lastState = window.undo_stack.pop();
+    window.redo_stack.push(currentState);
+    window.setCellColors(lastState);
+  }
+}
+
+function handleRedo() {
+  if (window.redo_stack.length > 0 && window.setCellColors) {
+    const currentState = window.getCellColors();
+    const state = window.redo_stack.pop();
+    window.undo_stack.push(currentState);
+    window.setCellColors(state);
+  }
+}
+
+//set global functions
+window.handleUndo = handleUndo;
+window.handleRedo = handleRedo;

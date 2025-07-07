0df8b1a26901c211874bd155da5508a618d3f996
Fatim-h <167449143+Fatim-h@users.noreply.github.com>
2025-07-08 03:27:37 +0500

    Update tools.js for handleUndo() and handleRedo() and <WorkspaceInfo />
    extended undo redo for palatte, added Workspace Info react component

--- tools.js
+++ tools.js
@@ -8,31 +8,86 @@ root2.render(<span>Color Hex: {input.value}</span>);
 input.addEventListener("change", (e) => {
   const currentColor = input.value;
   color_show.style.backgroundColor = input.value;
-  root2.render(<span>Color Hex: {currentColor}</span>); 
+  root2.render(<span>Color Hex: {currentColor}</span>);
 });
 
-//set global stacks
 window.undo_stack = [];
 window.redo_stack = [];
+window.palatte_undo_stack = [];
+window.palatte_redo_stack = [];
 
 function handleUndo() {
-  if (window.undo_stack.length > 0 && window.setCellColors) {
-    const currentState = window.getCellColors();
-    const lastState = window.undo_stack.pop();
-    window.redo_stack.push(currentState);
-    window.setCellColors(lastState);
+  if (
+    window.undo_stack.length > 0 &&
+    window.setCellColors &&
+    window.getCellColors &&
+    window.setPaletteColors &&
+    window.getPaletteColors
+  ) {
+    // restore palette
+    const currentPalette = window.getPaletteColors();
+    const lastPalette = window.palatte_undo_stack.pop();
+    if (lastPalette) {
+      window.palatte_redo_stack.push(currentPalette);
+      window.setPaletteColors(lastPalette);
+    }
+
+    // restore canvas
+    const currentCanvas = window.getCellColors();
+    const lastCanvas = window.undo_stack.pop();
+    window.redo_stack.push(currentCanvas);
+    window.setCellColors(lastCanvas);
+    updateLiveCanvas(lastCanvas);
   }
 }
 
 function handleRedo() {
-  if (window.redo_stack.length > 0 && window.setCellColors) {
-    const currentState = window.getCellColors();
-    const state = window.redo_stack.pop();
-    window.undo_stack.push(currentState);
-    window.setCellColors(state);
+  if (
+    window.redo_stack.length > 0 &&
+    window.setCellColors &&
+    window.getCellColors &&
+    window.setPaletteColors &&
+    window.getPaletteColors
+  ) {
+    // restore palette
+    const currentPalette = window.getPaletteColors();
+    const redoPalette = window.palatte_redo_stack.pop();
+    if (redoPalette) {
+      window.palatte_undo_stack.push(currentPalette);
+      window.setPaletteColors(redoPalette);
+    }
+
+    // restore canvas
+    const currentCanvas = window.getCellColors();
+    const redoCanvas = window.redo_stack.pop();
+    window.undo_stack.push(currentCanvas);
+    window.setCellColors(redoCanvas);
+    updateLiveCanvas(redoCanvas);
   }
 }
 
-//set global functions
 window.handleUndo = handleUndo;
 window.handleRedo = handleRedo;
+
+function WorkspaceInfo() {
+  const [dimensions, setDimensions] = React.useState({
+    width: window.currentCols || 0,
+    height: window.currentRows || 0,
+  });
+
+  React.useEffect(() => {
+    window.updateWorkspaceInfo = (newWidth, newHeight) => {
+      setDimensions({ width: newWidth, height: newHeight });
+    };
+  }, []);
+
+  return (
+    <p>
+      Width: {dimensions.width} <br />
+      Height: {dimensions.height}
+    </p>
+  );
+}
+
+const root4 = ReactDOM.createRoot(document.getElementById("width_height"));
+root4.render(<WorkspaceInfo />);
